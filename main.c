05da083419fc0d73526b278f7a25c31be7119252
abuchele <anna.buchele@students.olin.edu>
2016-11-30 22:06:47 -0500

    assmbler framework

--- main.c
+++ main.c
@@ -6,20 +6,64 @@
 #include <stdio.h>
 #include <string.h>
 
-void assemble(const char* instruction)
+int check_function(const char* instruction)
 {
     int result;
-    result = strncmp (instruction,"addi",4);
-    if (result == 0)
-    {
-        printf("%d\n", result);
-        printf("%s\n", "add immediate");
+    if(strcmp(instruction,"add") == 0){
+        result = 0;
+        goto end;
     }
-    printf("%d\n", result);
+    if(strcmp(instruction,"lw") == 0){
+        result = 1;
+        goto end;
+    }
+    if(strcmp(instruction,"sw") == 0){
+        result = 2;
+        goto end;
+    }
+    if(strcmp(instruction,"j") == 0){
+        result = 3;
+        goto end;
+    }
+    if(strcmp(instruction,"jr") == 0){
+        result = 4;
+        goto end;
+    }
+    if(strcmp(instruction,"jal") == 0){
+        result = 5;
+        goto end;
+    }
+    if(strcmp(instruction,"bne") == 0){
+        result = 6;
+        goto end;
+    }
+    if(strcmp(instruction,"xori") == 0){
+        result = 7;
+        goto end;
+    }
+    if(strcmp(instruction,"sub") == 0){
+        result = 8;
+        goto end;
+    }
+    if(strcmp(instruction,"slt") == 0){
+        result = 9;
+        goto end;
+    }
+    result = -1;
+    end:
+        return result;
 }
 
+char** parse_instr(char* instruction){
+    char* result;
+    while((result = strsep(&instruction, " "))) printf("%s\n",result);
+    printf("%s\n", result);
+
+}
 
 int main() {
-    assemble("addi s");
-    return 0;
+    int i = check_function("slt");
+    char ** f = parse_instr("slt $t3 $t4 $t1");
+    printf("%d\n", f);
+    return i;
 }
