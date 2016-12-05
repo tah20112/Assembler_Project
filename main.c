5899786d7323fe8bac3e248e2fd2b2300ed5c3eb
abuchele <anna.buchele@students.olin.edu>
2016-12-05 10:46:26 -0500

    Commenting, formatting

--- main.c
+++ main.c
@@ -3,13 +3,14 @@
 #include <stdlib.h>
 #include <stddef.h>
 
-int instr_type = -1; //0 is R; 1 is I; 2 is J.
+int instr_type = -1;          // 0 is R; 1 is I; 2 is J.
 
 
-int j_type(char* instruction[2]);
 
+//This function prints the binary of a hex input
+//Input: hex number
+//Output: none (prints binary number)
 void to_binary(int n){
-
     while (n){
         if (n & 0x80000000)
             printf("1");
@@ -19,7 +20,10 @@ void to_binary(int n){
     }
 }
 
-
+//This function takes the function instruction and
+//  checks it and outputs the correct opcodes and function codes
+//Input: string containing function ('add','j','xori',etc...)
+//Output: 32-bit opcode and, if used, function code
 int check_function(const char* instr){
     int result = 0;
     int funct = 0;
@@ -84,6 +88,10 @@ int check_function(const char* instr){
         return result;
 }
 
+//This function takes in the full mips instruction
+//  and separates it into the four instruction strings
+//Input: string containing full mips instruction ("add $t1 $t2 $s1)
+//Output: string with pointer containing separated instruction ("add" "$t1" "$t2" "$s1")
  char* *parse_instr(char *instruction){
      char *word;
      char* *output = malloc(5);
@@ -97,36 +105,24 @@ int check_function(const char* instr){
  }
 
 
-int check_instruction(char* instruction[4]) {
-    int i = 0;
-    switch(instr_type) {
-        case 0:
-            i = r_type(instruction);
-            break;
-        case 1:
-            i = i_type(instruction);
-            break;
-        case 2:
-            i = j_type(instruction);
-            break;
-    }
-    return i;
-}
-
-
 //Assembly code comes in, machine code comes out.
 // OPCODE RS RT RD SHIFT FUNCT
 // 000000  5  5  5  5     6
-//deal with all the registers and stuff that is not the opcode.
-//bit shift, and return...
-int r_type(char* instruction[3]){
 
-    int n = 1;
-    int reg_codes[3];
-    while (n < 4) {
-        int a = n-1;
-
+//This function takes in the full parsed mips instruction
+//  and outputs the correct register codes for an r-type instruction
+//Input: parsed string with pointer containing separated instruction ("add" "$t1" "$t2" "$s1")
+//Output: 26-bit machine code corresponding to the correct registers used
+int r_type(char* instruction[4]){
+    int n = 1;          //start at 1st register, which is 2nd in mips instruction
+    int reg_codes[3];   //create array to place the machine code for each register
+    while (n < 4) {     //run it for each of the three registers/immediates
+        int a = n-1;    //just to avoid calculating [n-1] every time the array is called
         char* reg_letter;
+
+        //First, run strncmp on the first two letters with each register letter
+        //if strncmp(instruction[n], $(letter), 2) == 0, the first two characters are $(letter).
+
         if (strncmp(instruction[n], "$s", 2) == 0) {
             reg_letter = "$s";
         }
@@ -148,55 +144,62 @@ int r_type(char* instruction[3]){
         else if (strncmp(instruction[n], "$g", 2) == 0) {
             reg_letter = "$f";
         }
-        else if (strncmp(instruction[n], "$g", 2) == 0) {
+        else if (strncmp(instruction[n], "$r", 2) == 0) {
             reg_letter = "$r";
         }
         else if (strncmp(instruction[n], "$z", 2) == 0) {
-            reg_letter = "$";
+            reg_letter = "$";   //set to null reg_letter value
             reg_codes[a] = 0b00000;
+            //if the register call starts with $z, it must be the zero register
+            //and we can just fill in reg_codes[a] here.
         }
         else{
-            reg_letter = "$";
-            char* s = instruction[n];
-            sscanf(s, "%x", &reg_codes[a]);
+            //if none of the above are correct, then we must be dealing with an immediate
+            reg_letter = "$";               //set to null reg_letter value
+            char* s = instruction[n];       //do string convert from hex value to integer
+            sscanf(s, "%x", &reg_codes[a]); //and save it to the reg_codes[a]
         }
+
+        //if we got a reg_letter earlier, we can run the strncmp to get the value for the next
+        //  character after the reg_letter. We use that value to find the full register address.
         int i = strncmp(instruction[n], reg_letter, 3);
 
+        //calculated equations for the difference between strncmp return value and corresponding register address
         if (reg_letter == "$t"){
             if (i == 0x38){
-                reg_codes[a] = 24;
+                reg_codes[a] = 0x18;
             }
             else if (i == 0x39){
-                reg_codes[a] = 25;
+                reg_codes[a] = 0x19;
             }
             else {
-                reg_codes[a] = (i - 0x30) + 0x8;
+                reg_codes[a] = (i - 0x28);
             }
         }
         else if (reg_letter == "$s") {
-            if (i == 112) {
-                reg_codes[a] = 29;
+            if (i == 0x70) {
+                reg_codes[a] = 0x1d;
             }
             else{
-                reg_codes[a] = (i-0x30) + 16;
+                reg_codes[a] = (i-0x1a);
             }
         }
         else if (reg_letter == "$v") {
-            if (i == 49) {
-                reg_codes[a] = 2;
+            if (i == 0x31) {
+                reg_codes[a] = 0x2;
             }
-            else if (i == 50){
-                reg_codes[a] = 3;
+            else if (i == 0x32){
+                reg_codes[a] = 0x3;
             }
         }
         else if (reg_letter == "$a") {
             if (i == 116){
                 reg_codes[a] = 1;
             }
-            reg_codes[a] = (i - 0x30) + 4;
+            reg_codes[a] = (i - 0x2c);
         }
         else if (reg_letter == "$k") {
-            reg_codes[a] = (i - 0x30) + 26;
+            reg_codes[a] = (i - 0x16);
         }
         else if (reg_letter == "$g") {
             reg_codes[a] = 28;
@@ -223,9 +226,10 @@ int r_type(char* instruction[3]){
 
 }
 
-
-
-
+//This function takes in the full parsed mips instruction
+//  and outputs the correct register codes for an i-type instruction
+//Input: parsed string with pointer containing separated instruction ("addi" "$t1" "$t2" "45")
+//Output: 26-bit machine code corresponding to the correct registers and immediate used
 int i_type(char* instruction[4]){
     int n = 1;
     int reg_codes[3];
@@ -323,8 +327,12 @@ int i_type(char* instruction[4]){
         return reg_full;
 }
 
+//This function takes in the full parsed mips instruction
+//  and outputs the correct register codes for an i-type instruction
+//Input: parsed string with pointer containing separated instruction ("j" "45")
+//Output: 26-bit machine code corresponding to the correct registers and immediate used
 int j_type(char* instruction[2]){
-    
+
     int reg_codes;
     char* s = instruction[1];
     sscanf(s, "%x", &reg_codes);
@@ -333,10 +341,34 @@ int j_type(char* instruction[2]){
     return immediate;
 }
 
+//This function takes in the full parsed mips instruction
+//  and according to the instruction type (global variable instr_type, assigned in check_function)
+//  runs the correct type of instruction parser
+//Input: parsed string with pointer containing separated instruction ("addi" "$s1" "$t2" "45")
+//Output: 26-bit machine code corresponding to the correct registers and immediate used
+int check_instruction(char* instruction[4]) {
+    int i = 0;
+    switch(instr_type) {
+        case 0:
+            i = r_type(instruction);
+            break;
+        case 1:
+            i = i_type(instruction);
+            break;
+        case 2:
+            i = j_type(instruction);
+            break;
+        case -1:
+            break;
+        default:break;
+    }
+    return i;
+}
 
-
-
-
+//This is the main function of the  program
+//  it runs all the other functions
+//Input: none
+//Output: 32-bit machine code corresponding to the correct functions, registers, and immediate used
 int main() {
     char* instruction = "j 450";
     char *instr = strdup(instruction);
@@ -350,5 +382,5 @@ int main() {
 
     to_binary(full);
 
-    return 0;
+    return full;
 }
