8800e29e79ed58dd9cd02c1c3e6f0ff8cea7e409
Zarin Bhuiyan <zarin.bhuiyan@students.olin.edu>
2016-12-14 16:07:52 -0500

    Adding whatver i have

---
+++ wrapper.py
@@ -0,0 +1,6 @@
+# use syscalls to take from assembler and feed to cpu
+from subprocess import call
+
+# subprocess.call(args, *, stdin=None, stdout=None, stderr=None, shell=False)
+
+subprocess.call(["....."])
